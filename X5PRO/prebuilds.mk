baa66ea1c907defaf3a8cdfacbc8f4f94bfe3ed1
SnowCatPDA <snowcat@vpro.ru>
2016-05-04 17:33:20 +0500

    UPDATE config

--- X5PRO/prebuilds.mk
+++ X5PRO/prebuilds.mk
@@ -1,12 +1,17 @@
 ########################################################
 # GAPPS apps
 GAPPS_VARIANT := pico
+GAPPS_FORCE_PACKAGE_OVERRIDES := true
+#GAPPS_FORCE_MATCHING_DPI := true
+#DONT_DEXPREOPT_PREBUILTS := true
+
 PRODUCT_PACKAGES += \
                     GoogleTTS \
                     GoogleHome \
                     CalendarGooglePrebuilt \
                     LatinImeGoogle \
-                    Chrome
+                    Chrome \
+                    Velvet
 
 ifneq ($(WITH_DEXPREOPT), yes)
 
@@ -20,32 +25,20 @@ ifneq ($(WITH_DEXPREOPT), yes)
 #                    TranslatePrebuilt
 endif
 
-#                    PrebuiltDeskClockGoogle \
-#                    WebViewGoogle \
-#                    GoogleEarth \
+#PRODUCT_PACKAGES += \
+#	busybox
+
+#	QuickSearchBox \
+#	PicoTts \
+#	Browser \
+#	LatinIME \
+#	Email \
+#	Calendar \
+#	MtkQuickSearchBox \
+#	MtkCalendar \
+#	MtkBrowser \
+#       SetupWizard \
+#       webview \
 
 $(call inherit-product-if-exists, vendor/google/build/opengapps-packages.mk)
-
-#PRODUCT_PACKAGES += \
-#        Superuser
-#	busybox
-#	su \
-
-REMOVE_APP := \
-	QuickSearchBox \
-	PicoTts \
-	Browser \
-	LatinIME \
-	Email \
-	Calendar \
-	MtkQuickSearchBox \
-	MtkCalendar \
-	MtkBrowser \
-        SetupWizard \
-
-#	DeskClock \
-#        webview \
-
-# NOT WORK YET
-PRODUCT_PACKAGES := $(filter-out $(REMOVE_APP),$(PRODUCT_PACKAGES))
 
